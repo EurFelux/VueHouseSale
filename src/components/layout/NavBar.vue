2a9338f765a4d976569067cfd18829c240c3684c
zjphantom <eurfelux@gmail.com>
2023-10-29 14:02:58 +0800

    fix bug

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -133,29 +133,18 @@ const accentColor = computed(() => publicStore.accentColor)
 const isLogin = computed(() => userStore.isLogin)
 const avatar = computed(() => userStore.avatar)
 
-// 判断是否为mobileMode
-const mobileModeThreshold = 1024;
-onBeforeMount(() => {
-    publicStore.setMobileMode(window.innerWidth < mobileModeThreshold)
-})
-
 // 适应移动端，设置垂直导航栏
 const mobileMode = computed(() => publicStore.mobileMode)
 const menuMode = computed(() => mobileMode.value ? 'vertical' : 'horizontal')
-const isCollapsed = ref(true)
+const isCollapsed = ref(mobileMode.value)
 const collapseIcon = computed(() => isCollapsed.value ? Expand : Fold)
-
 
 function toggleCollapse() {
     isCollapsed.value = !isCollapsed.value;
 }
 
 watch(mobileMode, () => {
-    if (!mobileMode.value) {
-        isCollapsed.value = false;
-    } else {
-        isCollapsed.value = true;
-    }
+    isCollapsed.value = mobileMode.value
 })
 
 
